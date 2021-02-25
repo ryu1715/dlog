88d1de54f5f01d86dbaa6cecf804c100be8efb97
比嘉流来 <ryuuku@hikaryuurainoMacBook-Pro.local>
2021-02-26 08:19:05 +0900

    テンプレート修正

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,11 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8pfqf9scuku&=_qz!v(7(z9y@q&xc+u!-vi_kvi4lv)6zr7%$f'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -41,7 +40,6 @@ INSTALLED_APPS = [
     'myapp',
     'django.contrib.sites',
     'sitemanage',
-
 ]
 
 SITE_ID = 1
